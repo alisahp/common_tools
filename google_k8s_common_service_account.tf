737b2155d5a7006a4842046335f773c563f2a914
heromahir <mhr2495@gmail.com>
2020-03-22 14:18:58 -0500

    common service updated

--- google_k8s_common_service_account.tf
+++ google_k8s_common_service_account.tf
@@ -1,20 +1,20 @@
-resource "kubernetes_service_account" "mybestsea_service_account" {
+resource "kubernetes_service_account" "common_service_account" {
   depends_on = [
     "kubernetes_namespace.service_tools",
     "kubernetes_service_account.tiller",
     "kubernetes_secret.tiller"
     ]
   metadata {
-    name = "mybestsea-service-account"
+    name = "common-service-account"
     namespace = "${var.deployment_environment}"
   }
   secret {
-    name = "${kubernetes_secret.mybestsea_service_account_secret.metadata.0.name}"
+    name = "${kubernetes_secret.common_service_account_secret.metadata.0.name}"
   }
   automount_service_account_token = true
 }
 
-resource "kubernetes_secret" "mybestsea_service_account_secret" {
+resource "kubernetes_secret" "common_service_account_secret" {
   depends_on = [
     "kubernetes_namespace.service_tools",
     "kubernetes_service_account.tiller",
